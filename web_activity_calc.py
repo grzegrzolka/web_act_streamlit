bfdc44628a6433856e53e61e9ff4f348bfded894
grzegrzolka <andygrzegrzolka@gmail.com>
2025-11-09 15:56:55 +0100

    Changed file with isotopes data

--- web_activity_calc.py
+++ web_activity_calc.py
@@ -40,15 +40,28 @@ st.markdown("###### contact: :blue[andrzej.grzegrzolka@zuop.gov.pl]" )
 # title of the app
 st.subheader('Isotope activity calculator')
 
-path = Path(__file__).parent / "Isotope.csv"
+#path = Path(__file__).parent / "Isotope.csv"
+## dataframe loading
+#df = pd.read_csv(path , sep = ";", index_col = 0, 
+#        names = ["Name", "AExemptionConcentr", "ExemptionActivity", "HalfLife"])
+#df.loc[df['HalfLife'] == "#N/D!", 'HalfLife'] = '0'
+#df['HalfLife'] = df['HalfLife'].str.replace(',', '.').astype(float)
+
+
+path = Path(__file__).parent / "IA1_A2_TS_D_ActConcente.csv"
 # dataframe loading
-df = pd.read_csv(path , sep = ";", index_col = 0, 
-        names = ["Name", "ExemptionConcentr", "ExemptionActivity", "HalfLife"])
-df.loc[df['HalfLife'] == "#N/D!", 'HalfLife'] = '0'
-df['HalfLife'] = df['HalfLife'].str.replace(',', '.').astype(float)
+df = pd.read_csv('A1_A2_TS_D_ActConcent.csv', sep = ";", index_col = 0, names = ["Activity concentration for exempt material (kBq/kg)", 
+                                                                                 "Activity limit for an exempt consignment (Bq)", "A1 [TBq]", "A2 [TBq]", 
+                                                                                 "Transport Security Threshold [TBq]", "D-Value [TBq]", "Desintegration [Years]"])
+df['A1 [TBq]'] = df['A1 [TBq]'].str.replace(',', '.')
+df['A2 [TBq]'] = df['A2 [TBq]'].str.replace(',', '.')
+df['Transport Security Threshold [TBq]'] = df['Transport Security Threshold [TBq]'].str.replace(',', '.')
+df['D-Value [TBq]'] = df['D-Value [TBq]'].str.replace(',', '.')
+df['Desintegration [Years]'] = df['Desintegration [Years]'].str.replace(',', '.')
+#st.write(df)
 
 # visualize dataframe
-st.write(df)
+#st.write(df)
 
 # input dates
 start_date = st.date_input("Enter beginng date:", min_value="1900-01-01", max_value="2300-01-01")
@@ -97,17 +110,19 @@ data = []
 
 x = range(elapsed_months)
 for isotope in st.session_state['isotopes']:
+    act_conc_limit = float(df.loc[isotope.isotope_name]["Activity concentration for exempt material (kBq/kg)"])
+    exemption_act = float(df.loc[isotope.isotope_name]["Activity limit for an exempt consignment (Bq)"])
     #calculate activity
-    halflife = float(df.loc[isotope.isotope_name]["HalfLife"])
-    calc_activity = round(isotope.activity * pow(math.e, -(math.log(2) * (elapsed_years / halflife))),2)
-    calc_3_year_activity = round(isotope.activity * pow(math.e, -(math.log(2) * (3 / halflife))),2)
-    data.append([isotope.isotope_name, halflife, isotope.activity, calc_activity, calc_3_year_activity])
+    halfLife = float(df.loc[isotope.isotope_name]["Desintegration [Years]"])
+    calc_activity = round(isotope.activity * pow(math.e, -(math.log(2) * (elapsed_years / halfLife))),2)
+    calc_3_year_activity = round(isotope.activity * pow(math.e, -(math.log(2) * (3 / halfLife))),2)
+    data.append([isotope.isotope_name, act_conc_limit, exemption_act, halfLife, isotope.activity, calc_activity])
     
     #create plot
     activities_by_month = []
     for month in range(elapsed_months):
         time = month / 12 
-        activities_by_month.append(round(isotope.activity * pow(math.e, - (math.log(2) * (time / halflife) )),2))
+        activities_by_month.append(round(isotope.activity * pow(math.e, - (math.log(2) * (time / halfLife) )),2))
     
     plt.plot(x, activities_by_month, label = isotope.isotope_name)
 
@@ -128,6 +143,9 @@ st.pyplot(plt)
 
 st.divider()  
 
-selected_isotopes_df = pd.DataFrame(data, columns=['Isotope', 'Halflife', 'Begining Act', 'Calculated Act', 'Act after 3 Years'])
-st.write(selected_isotopes_df )
+selected_isotopes_df = pd.DataFrame(data, columns=['Isotope', 'Activity concentration for exempt material (kBq/kg)', 'Activity limit for an exempt consignment (Bq)', 'Desintegration [Years]', 'Begining Act', 'Calculated Act'])
+#st.write(selected_isotopes_df )
 
+#st.dataframe(selected_isotopes_df.T,  column_config= {"_index": st.column_config.Column("", width = "None")}) 
+
+st.dataframe(selected_isotopes_df.T,  column_config= {"_index": st.column_config.Column("", width = "medium")})  #display dataframe with selected isotopes
