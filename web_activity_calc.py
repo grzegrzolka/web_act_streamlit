393f56c85d56985a67db40611f4aad1753706bbd
grzegrzolka <andygrzegrzolka@gmail.com>
2025-11-10 18:30:15 +0100

    Blue title

--- web_activity_calc.py
+++ web_activity_calc.py
@@ -1,26 +1,3 @@
-
-# import streamlit as st
-
-
-# if 'my_lst' not in st.session_state:
-#     st.session_state['my_lst'] = []
-
-# def manager():
-#     with st.expander("Example"):
-#         #user_input = st.text_input("Enter a key")
-#         user_input = st.selectbox(label="podaj izotop", options = ["pierwszy", "drugi", "trzeci"])
-#         add_button = st.button("Add", key='add_button')
-#         if add_button:
-#             if len(user_input) > 0:
-#                 st.session_state['my_lst'] += [user_input]
-#                 st.write( st.session_state['my_lst'] )
-#             else:
-#                 st.warning("Enter text")
-
-
-#manager()
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -34,19 +11,9 @@ class Isotopes_in_Waste:
         self.isotope_name = isotope
         self.activity = activity
 
-st.markdown("###### author: :blue[Andrzej Grzegrzłka]" )
-st.markdown("###### contact: :blue[andrzej.grzegrzolka@zuop.gov.pl]" )
-
 # title of the app
-st.subheader('Isotope activity calculator')
-
-#path = Path(__file__).parent / "Isotope.csv"
-## dataframe loading
-#df = pd.read_csv(path , sep = ";", index_col = 0, 
-#        names = ["Name", "AExemptionConcentr", "ExemptionActivity", "HalfLife"])
-#df.loc[df['HalfLife'] == "#N/D!", 'HalfLife'] = '0'
-#df['HalfLife'] = df['HalfLife'].str.replace(',', '.').astype(float)
-
+st.subheader(':blue[Isotope activity calculator]')
+st.markdown('###### :green[This application displays some data about choosen isotopes, calculate their activity and displays the activity decrease within a given time period.]')
 
 path = Path(__file__).parent / "IA1_A2_TS_D_ActConcente.csv"
 # dataframe loading
@@ -58,10 +25,6 @@ df['A2 [TBq]'] = df['A2 [TBq]'].str.replace(',', '.')
 df['Transport Security Threshold [TBq]'] = df['Transport Security Threshold [TBq]'].str.replace(',', '.')
 df['D-Value [TBq]'] = df['D-Value [TBq]'].str.replace(',', '.')
 df['Desintegration [Years]'] = df['Desintegration [Years]'].str.replace(',', '.')
-#st.write(df)
-
-# visualize dataframe
-#st.write(df)
 
 # input dates
 start_date = st.date_input("Enter beginng date:", min_value="1900-01-01", max_value="2300-01-01")
@@ -126,7 +89,6 @@ for isotope in st.session_state['isotopes']:
     
     plt.plot(x, activities_by_month, label = isotope.isotope_name)
 
-# 
 plt.ticklabel_format(style='plain')
 plt.xlabel("Time (months)")
 plt.ylabel("Activity")
@@ -135,17 +97,10 @@ plt.legend()  #add a legend
 #plt.savefig('chart.png') #/Users/andy/Desktop/Programming/Python/JacekIsot/chart.png
 st.pyplot(plt)
 
-#print dataframe
-#st.write("Data początkowa", start_date )
-#st.write("Data końcowa", end_date )
-#st.write("Elapsed years: ", round(elapsed_years,2))
-#st.write("Elapsed months: ", elapsed_months)
-
 st.divider()  
-
-selected_isotopes_df = pd.DataFrame(data, columns=['Isotope', 'Activity concentration for exempt material (kBq/kg)', 'Activity limit for an exempt consignment (Bq)', 'Desintegration [Years]', 'Begining Act', 'Calculated Act'])
-#st.write(selected_isotopes_df )
-
-#st.dataframe(selected_isotopes_df.T,  column_config= {"_index": st.column_config.Column("", width = "None")}) 
-
+selected_isotopes_df = pd.DataFrame(data, columns=['Isotope', 'Act concent for exempt material (kBq/kg)', 'Act limit for exempt mat (Bq)', 'Desintegration [Years]', 'Begining Activity', 'Calculated Activity'])
 st.dataframe(selected_isotopes_df.T,  column_config= {"_index": st.column_config.Column("", width = "medium")})  #display dataframe with selected isotopes
+
+st.divider() 
+st.text("Made by: Andrzej Grzegrzółka")
+st.markdown("contact: :blue[andrzej.grzegrzolka@zuop.gov.pl]" )
